de13fbc9f97f32eca7433bc208ea2f060c747059
SachinSharma22 <s830sharma@gmail.com>
2025-03-08 14:38:00 +0530

    learn true and false value

--- calc.js
+++ calc.js
@@ -27,15 +27,16 @@ function writeToLog(operationIdentifier, prevResult, operationNumber, newResult)
 }
 
 function calculateResult(calculationType){
+    const enteredNumber = getUserNumberInput();
 if(calculationType !== 'ADD' &&
     calculationType !== 'SUBTRACT' &&
     calculationType !== 'MULTIPLY' &&
-    calculationType !== 'DIVIDE' 
+    calculationType !== 'DIVIDE' ||
+    !enteredNumber
 ) {
     return;
 }
 
-    const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     let mathOperator;
     if(calculationType === 'ADD'){
